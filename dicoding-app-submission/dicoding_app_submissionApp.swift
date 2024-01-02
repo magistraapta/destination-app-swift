5f79d98f9da443002111d9a63fb5c5d90d720339
magistraapta <magsitraagis@gmail.com>
2024-01-02 13:31:54 +0800

    Initial Commit

---
+++ dicoding-app-submission/dicoding_app_submissionApp.swift
@@ -0,0 +1,17 @@
+//
+//  dicoding_app_submissionApp.swift
+//  dicoding-app-submission
+//
+//  Created by Magistra Apta on 02/01/24.
+//
+
+import SwiftUI
+
+@main
+struct dicoding_app_submissionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
